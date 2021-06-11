a8f9a06e37d80fb082dbf78e0f7e278c8322cfd5
aakashdeveloper <ahanda205@gmail.com>
2021-06-11 07:41:34 +0530

    mymobi

--- mymobi/App.js
+++ mymobi/App.js
@@ -1,12 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, Button, Switch,ImageBackground } from 'react-native';
 
-export default function App() {
+const image = { uri: "https://reactjs.org/logo-og.png" };
+
+function App() {
+  const [OutputText, setText] = useState('Test Native App');
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <ImageBackground source={image} style={styles.image}>
+        <Text style={styles.text}>Inside</Text>
+      </ImageBackground>
+      <Text>{OutputText}</Text>
+      <Button title="Click me" onPress={()=>{setText('Text Change')}}/>
+      <Button title="Revert me" onPress={()=>{setText('Test Native App')}}/>
+      <Switch
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
     </View>
   );
 }
@@ -18,4 +34,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000a0"
+  }
 });
+
+export default App;
